de5aa5a9fdff031f855e1bfa9124f41604d8b7d4
Japneet Sahni <japneetsahni30@gmail.com>
2020-06-07 12:48:49 +0530

    Changing modules repo name to support terraform cloud

--- modules_registry/projects/M/main.tf
+++ modules_registry/projects/M/main.tf
@@ -1,4 +1,4 @@
 module "ec2module" {
-    source = "../../modules/ec2"
+    source = "https://github.com/japneet-sahni/Terraform-HandsOn/tree/master/terraform-aws-ec2"
     # instance_type = "t2.micro"
 }
