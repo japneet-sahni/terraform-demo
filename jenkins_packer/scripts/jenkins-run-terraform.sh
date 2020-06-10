522800723a3f7ca6f490195397b77979ad03b81f
Japneet Sahni <japneetsahni30@gmail.com>
2020-06-10 13:31:06 +0530

    Adding jenkins packer terraform integration

--- jenkins_packer/scripts/jenkins-run-terraform.sh
+++ jenkins_packer/scripts/jenkins-run-terraform.sh
@@ -2,6 +2,7 @@
 set -ex
 AWS_REGION="us-east-1"
 
+S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
 terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
